ae6affd57602661601960bccc2c411707b069bc8
polatgray <polatgraywebveodeme@gmail.com>
2025-01-03 23:22:24 +0300

    j

--- src/components/HomeComponents/Home.jsx
+++ src/components/HomeComponents/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
                     <div className="flex flex-col h-full">
                         <div className="flex items-center flex-col mt-12 absolute top-0 w-full sm:pt-4 pt-16">
                             <p className="inter-600  2xl:text-8xl  lg:text-6xl sm:text-5xl text-4xl mainLabel tracking-wide text-shadow-yellow animated-text ">POLAT GRAY</p>
-                            <p className="opacity-90 inter-500 tracking-widest-more mt-5 md:text-lg text-xs text-white helperLabel">WELCOME TO PARADISE</p>
+                            <p className="opacity-90 inter-500 tracking-widest-more mt-5 md:text-lg text-xs text-white helperLabel">WELCOME TO PARADIS</p>
                         </div>
                        <div className="flex justify-center items-center flex-col h-full mt-5">
                             <img src={MainPhoto} className="defaultImg object-cover 2xl:w-[800px] xl:w-[800px] lg:w-[700px] md:w-[500px] sm:w-[400px] w-[300px] box-shadow-yellow 2xl:h-[220px] xl:h-[220px] lg:h-[180px] md:h-[160px] sm:h-[120px] sm:h-[100px] h-[100px]  rounded-lg object-center" alt="Polat Gray" />
