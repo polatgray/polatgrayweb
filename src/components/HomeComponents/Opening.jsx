e54e22fc8a92e2fd4259d43c823c4118724ba57f
polatgray <polatgraywebveodeme@gmail.com>
2025-01-06 23:01:21 +0300

    loading fix

--- src/components/HomeComponents/Opening.jsx
+++ src/components/HomeComponents/Opening.jsx
@@ -4,8 +4,8 @@ const Opening = () => {
     return (
         <div className="fixed w-full h-full bg-black z-20 flex items-center justify-center">
             <div className="relative flex flex-col items-center justify-center">
-                <p className="animated-text text-9xl pg-text ">PG</p>
-                <p className="dm-serif-text-regular text-white sm:text-2xl text-lg animated-text animated-text-opening-label text-center">The Best <strong> Charismatic Millionaire </strong> in the World</p>
+                <p className="animated-text sm:text-9xl text-7xl pg-text ">PG</p>
+                <p className="dm-serif-text-regular text-white sm:text-2xl text-2xl px-2 animated-text animated-text-opening-label text-center">World’s Best Charismatic Millionaire</p>
             </div>
         </div>
     );
