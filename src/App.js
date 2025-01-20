567c5f070d30586315eb80560e32ec060b10d320
polatgray <polatgraywebveodeme@gmail.com>
2025-01-20 16:15:39 +0300

    vercel analyttics enable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { LoadingProvider } from "./Context/LoadingContext";
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
