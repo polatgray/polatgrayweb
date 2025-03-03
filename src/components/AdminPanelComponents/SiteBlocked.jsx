7418c284bc3a5c9b49055b6a4e65aab356b88213
sekullar <asdkralxasd@gmail.com>
2025-03-03 17:01:05 +0300

    fix

--- src/components/AdminPanelComponents/SiteBlocked.jsx
+++ src/components/AdminPanelComponents/SiteBlocked.jsx
@@ -3,10 +3,10 @@ import Logo from "../../images/logo3.svg"
 const siteBlocked = () => {
     return(
         <>
-            <div className="flex sm:flex-row flex-col fixed justify-center items-center h-screen w-full gap-8 bg-white">
+            <div className="flex sm:flex-row sm:items-start items-center flex-col fixed justify-center items-center h-screen w-full gap-8 bg-white">
                 <img src={Logo} className="w-[80px]" alt="Seku Software Logo" />
                 <div className="bg-gray-900 w-[60px] sm:w-[3px] rounded-lg h-[3px] sm:h-[60px]"></div>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-3 ">
                     <p className="inter-600 text-3xl">Bu siteye erişim şu anda mevcut değil.</p>
                     <p className="inter-500 text-2xl">Access to this site is currently unavailable.</p>
                 </div>
